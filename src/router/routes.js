7e6737f980680852e912e1c6d97e4a35a089d778
moliv <molivramento@gmail.com>
2023-11-23 18:14:28 -0300

    adding auth

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,25 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/EmptyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/auth/LoginPage.vue') },
+      { path: 'login', name: 'login', component: () => import('pages/auth/LoginPage.vue') }
+    ]
+  },
+
+  {
+    path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'products', component: () => import('src/pages/products/ProductsPage.vue') },
       { path: 'addproduct', component: () => import('src/pages/products/AddProductPage.vue') },
       { path: 'putproduct', component: () => import('src/pages/products/PutProductPage.vue') }
-    ]
+    ],
+    meta: {
+      requiresAuth: true
+    }
   },
 
   // Always leave this as last one,
