c704f3c8cc26313b346d19b3619119016616b93c
david nascimento <viniciosdavid1@gmail.com>
2023-11-30 07:22:32 -0300

    concluindo função de cadastro de vendas

--- src/stores/PdvStore.js
+++ src/stores/PdvStore.js
@@ -7,7 +7,8 @@ export const usePDVStore = defineStore('pdv', {
     itens: ref([]),
     product: useAPI('products'),
     transation: useAPI('transation'),
-    item: useAPI('itens')
+    item: useAPI('itens'),
+    transationId: ref('')
   }),
   getters: {
     doubleCount: (state) => state.counter * 2
@@ -17,11 +18,13 @@ export const usePDVStore = defineStore('pdv', {
       const Transation = await this.transation.post({})
       for (let i = 0; i < this.itens.length; i++) {
         const Product = await this.product.getById(this.itens[i].uuid)
-        const responseData = await this.item.post({
-          total_amount: 0,
-          product: Product,
+        const payload = {
+          total_amount: Product[0].quantity,
+          product: Product[0],
           transation: Transation
-        })
+        }
+        console.log(payload)
+        const responseData = await this.item.post(payload)
         console.log(responseData)
       }
     }
